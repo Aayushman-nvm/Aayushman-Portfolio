3bd391cbe792cdc7b82cece15ea590fc7f132d57
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-06-09 20:35:41 +0530

    App routing and animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion';
+
 import Layout from './components/Layout'
+import PageWrapper from './components/PageWrapper';
+
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Resume from './pages/Resume'
 import Contact from './pages/Contact'
 function App() {
 
+  const location = useLocation();
+
   return (
     <div className='md:flex items-center justify-center min-h-screen bg-[#0e1013] md:p-24'>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/resume' element={<Resume />} />
-          <Route path='/Contact' element={<Contact />} />
-        </Route>
-      </Routes>
+      <AnimatePresence mode='wait'>
+        <Routes location={location} key={location.pathname}>
+          <Route element={<Layout />}>
+            <Route
+              path="/"
+              element={<PageWrapper><Home /></PageWrapper>}
+            />
+            <Route
+              path="/resume"
+              element={<PageWrapper><Resume /></PageWrapper>}
+            />
+            <Route
+              path="/resume"
+              element={<PageWrapper><Resume /></PageWrapper>}
+            />
+            <Route
+              path="/projects"
+              element={<PageWrapper><Projects /></PageWrapper>}
+            />
+            <Route
+              path="/contact"
+              element={<PageWrapper><Contact /></PageWrapper>}
+            />
+          </Route>
+        </Routes>
+      </AnimatePresence>
     </div>
   )
 }
